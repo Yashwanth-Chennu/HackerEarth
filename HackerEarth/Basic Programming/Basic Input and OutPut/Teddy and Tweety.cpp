670eba4f47f6a4c608b4ae01226f112f84284685
Yashwanth Chennu <yashwanthchennu1412@gmail.com>
2020-08-05 22:52:22 +0530

    Add files via upload

---
+++ HackerEarth/Basic Programming/Basic Input and OutPut/Teddy and Tweety.cpp
@@ -0,0 +1,21 @@
+https://github.com/Yashwanth-Chennu
+
+Problem Statement: https://www.hackerearth.com/practice/basic-programming/input-output/basics-of-input-output/practice-problems/algorithm/teddy-and-tweety/submissions/
+
+Solution: 
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	int n;
+	cin >> n;
+	if(n%3 == 0)
+		cout <<"YES";
+	else
+		cout << "NO"; 
+	return 0;
+}
+
+
+
+   
